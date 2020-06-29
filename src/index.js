5727437a05021e59d3b657c0fd8b049d59c487a3
Fabio Coppola <fabio.coppola@friendz-app.com>
2020-06-29 13:03:10 +0200

    resolve in case of error

--- src/index.js
+++ src/index.js
@@ -19,110 +19,117 @@ const defaultArgs = {
 };
 
 module.exports = {
-  setArguments: function(args) {
+  setArguments: function (args) {
     this.args = Object.assign({}, defaultArgs, args);
     this.args.WAIT_ALL_SERVERS = Boolean(this.args.WAIT_ALL_SERVERS);
     const toRetain = Number.parseInt(this.args.N_DEPLOYMENTS_TO_RETAIN, 10);
     this.args.N_DEPLOYMENTS_TO_RETAIN = Math.max(0, Number.isNaN(toRetain) ? 0 : toRetain);
   },
 
-  run: function() {
+  run: function () {
     return new Promise((resolve, reject) => {
-      const result = {};
+      try {
+        const result = {};
 
-      switch (this.args.DEPLOY_PHASE) {
-        case 'GIT_ARCHIVE':
-          const commitDir = path.join(this.args.DEPLOYMENTS_DIRECTORY, String(Date.now()));
-
-          try {
-            fs.mkdirSync(commitDir, {recursive: true});
-          } catch (e) {
-            if (e.code !== 'EEXIST') {
-              throw e;
+        switch (this.args.DEPLOY_PHASE) {
+          case 'GIT_ARCHIVE':
+            const commitDir = path.join(this.args.DEPLOYMENTS_DIRECTORY, String(Date.now()));
+  
+            try {
+              fs.mkdirSync(commitDir, { recursive: true });
+            } catch (e) {
+              if (e.code !== 'EEXIST') {
+                throw e;
+              }
             }
-          }
-
-          const gitArchive = child_process.spawn('git', [
-            'archive',
-            '--format=tar',
-            '--remote',
-	          this.args.REPOSITORY_URL,
-            '--worktree-attributes',
-            this.args.GIT_ARCHIVE_TREEISH,
-          ], {
-	          cwd: commitDir
-	        });
-
-	        const tarExtractArgs = ['-x'];
-
-          if (this.args.TAR_EXTRACT_EXCLUDE_PATHS) {
-            tarExtractArgs.push(...this.args.TAR_EXTRACT_EXCLUDE_PATHS.split(',').map(s => `--exclude=${s}`));
-          }
-
-          const tarExtract = child_process.spawn('tar', tarExtractArgs, {
-            cwd: commitDir
-          });
-
-          gitArchive.stdout.on('data', (data) => {
-            tarExtract.stdin.write(data);
-          });
-
-          gitArchive.once('close', (code) => {
-            result.gitArchiveStatusCode = code;
-
-            if (code !== 0) {
-              return resolve(result);
+  
+            const gitArchive = child_process.spawn('git', [
+              'archive',
+              '--format=tar',
+              '--remote',
+              this.args.REPOSITORY_URL,
+              '--worktree-attributes',
+              this.args.GIT_ARCHIVE_TREEISH,
+            ], {
+              cwd: commitDir
+            });
+  
+            const tarExtractArgs = ['-x'];
+  
+            if (this.args.TAR_EXTRACT_EXCLUDE_PATHS) {
+              tarExtractArgs.push(...this.args.TAR_EXTRACT_EXCLUDE_PATHS.split(',').map(s => `--exclude=${s}`));
             }
-
-            tarExtract.stdin.end();
-          });
-
-          tarExtract.once('exit', (code) => {
-            result.tarExtractStatusCode = code;
-
-            if (code !== 0) {
-              return resolve(result);
-            }
-
-            this.doProjectSymlinks(commitDir);
-
-            if (this.args.COMPOSER_COMMAND) {
-              const composerInstall = child_process.spawnSync(this.args.COMPOSER_PATH, [
-                      this.args.COMPOSER_COMMAND,
-                      '--no-ansi',
-                      '--no-interaction',
-                      '--no-plugins',
-                      '--no-dev',
-                      '--no-progress',
-                      '--no-suggest'
-                    ], {
-                cwd: commitDir
-              });
-
-              result.composerInstallStatusCode = composerInstall.status;
-
-              if (composerInstall.status !== 0) {
-	              return resolve(result);
-	            }
-	          }
-
-            if (this.args.WAIT_ALL_SERVERS) {
-              result.commitDir = commitDir;
-              result.DEPLOY_PHASE = 'ACTIVATE';
-	      return resolve(result);
-            } else {
-	      this.doAfterInstall(commitDir, result);
-	      return resolve(result);
-            }
-          });
-
-	  break;
-        case 'ACTIVATE':
-          this.doAfterInstall(this.args.commitDir, result);
-	  return resolve(result);
-	default:
-	  reject();
-	  break;
+  
+            const tarExtract = child_process.spawn('tar', tarExtractArgs, {
+              cwd: commitDir
+            });
+  
+            gitArchive.stdout.on('data', (data) => {
+              tarExtract.stdin.write(data);
+            });
+  
+            gitArchive.once('close', (code) => {
+              result.gitArchiveStatusCode = code;
+  
+              if (code !== 0) {
+                return resolve(result);
+              }
+  
+              tarExtract.stdin.end();
+            });
+  
+            tarExtract.once('exit', (code) => {
+              result.tarExtractStatusCode = code;
+  
+              if (code !== 0) {
+                return resolve(result);
+              }
+  
+              this.doProjectSymlinks(commitDir);
+  
+              if (this.args.COMPOSER_COMMAND) {
+                const composerInstall = child_process.spawnSync(this.args.COMPOSER_PATH, [
+                  this.args.COMPOSER_COMMAND,
+                  '--no-ansi',
+                  '--no-interaction',
+                  '--no-plugins',
+                  '--no-dev',
+                  '--no-progress',
+                  '--no-suggest'
+                ], {
+                  cwd: commitDir
+                });
+  
+                result.composerInstallStatusCode = composerInstall.status;
+  
+                if (composerInstall.status !== 0) {
+                  return resolve(result);
+                }
+              }
+  
+              if (this.args.WAIT_ALL_SERVERS) {
+                result.commitDir = commitDir;
+                result.DEPLOY_PHASE = 'ACTIVATE';
+                return resolve(result);
+              } else {
+                this.doAfterInstall(commitDir, result);
+                return resolve(result);
+              }
+            });
+  
+            break;
+          case 'ACTIVATE':
+            this.doAfterInstall(this.args.commitDir, result);
+            return resolve(result);
+          default:
+            return reject({
+              error_message: 'wrong arguments'
+            });
+        }
+      } catch (e) {
+        reject({
+          error: e.toString()
+        });
       }
     });
   },
